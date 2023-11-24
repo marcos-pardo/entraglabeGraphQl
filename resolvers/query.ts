90b06efff8875a8bf670bde1d27ee4f2e74787ee
marcos-pardo <101109006+marcos-pardo@users.noreply.github.com>
2023-11-24 18:44:35 +0100

    otro Cambio en mascota por nombre

--- resolvers/query.ts
+++ resolvers/query.ts
@@ -33,6 +33,19 @@ export const Query = {
       }
 
   },
+  nameMascota : async (_:unknown, args:{name?: string}): Promise<Array<Pet>> => {
+    const {name} = args;
+    // Si no se especifica la raza, se devuelven todas las mascotas, y si se especifica, solo las de esa raza
+    const mascotas = await MascotaModel.find(name ? {name} : {});
+    return mascotas.map((mascota) => {
+      return {
+        id: mascota._id.toString(),
+        name: mascota.name,
+        breed: mascota.breed,
+  }
+}
+)
+  }
 
 
 
